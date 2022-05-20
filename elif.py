4f5a94514ebdc6c69ad5a57b8d3126c5e16933d1
Chandra <210101011@mhs.udb.ac.id>
2022-05-20 13:32:49 +0700

    contoh materi elif

---
+++ elif.py
@@ -0,0 +1,9 @@
+menu_pilihan = input('Silahkan Pilih menu 1-3 : ')
+if menu_pilihan == '1':
+    print ('Anda Memiih Menu 1')
+elif menu_pilihan == '2':
+    print ('Anda Memiih Menu 2')
+elif menu_pilihan == '3':
+    print ('Anda Memiih Menu 3')
+else:
+    print ('Menu Tida Tersedia')
