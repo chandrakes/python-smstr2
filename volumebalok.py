0684883ad31349ec0a8099b470367b0d8867833f
Chandra <210101011@mhs.udb.ac.id>
2022-05-20 13:25:18 +0700

    menghitung volume balok

---
+++ volumebalok.py
@@ -0,0 +1,7 @@
+print('====Menghitung Volume Balok=====')
+panjang = int(input('Input Panjang Balok    : '))
+lebar = int(input  ('Input Lebar Balok      : '))
+tinggi = int(input ('Input Tinggi Balok     : '))
+volume = panjang*lebar*tinggi
+print('=========HASIL========')
+print('Volume Balok : ',volume)
