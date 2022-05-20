9583121c4ae8144483d1d58e38633aaf9e9a6d2c
Chandra <210101011@mhs.udb.ac.id>
2022-05-20 13:31:33 +0700

    contoh materi if

---
+++ if.py
@@ -0,0 +1,7 @@
+nilai = int(input('Masukan Nilai : '))
+if (nilai >= 5):
+    print('Selamat Anda Lulus')
+if (nilai > 10):
+    print('Selamat Anda Lulus dengan nilai diatas 10')
+if (nilai < 5):
+    print('Anda Tidak Lulus')
