03f544441ce6881f5da42a886d2a3e9e11aaf8f8
Chandra <210101011@mhs.udb.ac.id>
2022-05-20 13:37:41 +0700

    contoh if bersarang

---
+++ if_nest.py
@@ -0,0 +1,9 @@
+user = input('Masukan User Name : ')
+pas =  input('Masukan Password  : ')
+if user == 'chandra':
+    if pas == '123':
+        print('Selamat Datang')
+    else:
+        print('Password Salah')
+else:
+    print('Akun Tidak Ada')
